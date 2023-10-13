ceea69ab744b3b0dca4fc841d3c250ab78f984a7
Armaan Uddin <armaan.uddin@kcl.ac.uk>
2023-10-13 21:10:25 +0100

    Add method to display the first n prime numbers where n is positive

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if(number_of_primes < 1):
+        raise ValueError("Only positive numbers are allowed.")
+    
     list = []
+    primes_calculated = 0
+    number = 0
+
+    while primes_calculated < number_of_primes:
+        factors = 1
+
+        for i in range(1, number):
+            if number % i == 0:
+                factors += 1
+        
+        if factors == 2:
+            primes_calculated += 1
+            list.append(number)
+
+        number += 1
+
     return list
